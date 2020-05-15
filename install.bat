b156cdd6d70baa45061bb1e19a1c5c80622c7a86
aimmy_camtpc <noppon_aim@hotmail.com>
2020-05-15 13:24:35 +0700

    update script path form v4.0 to v0.4.14

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-cmd /k "D:\OpenCue\v4.0\venv\Scripts\activate & pip install -r requirements.txt & python setup.py install
+cmd /k "D:\OpenCue\v0.4.14\venv\Scripts\activate & pip install -r requirements.txt & python setup.py install
 
--- run_rqd.bat
+++ run_rqd.bat
@@ -3,4 +3,4 @@ set OPENCUE_SERVER=localhost
 set CUEBOT_HOSTS=%OPENCUE_SERVER%
 set CUEBOT_HOSTNAME=%OPENCUE_SERVER%
 set CUEBOT_HOSTNAME_OR_IP=%OPENCUE_SERVER%
-cmd /k "cd /d D:\OpenCue\v4.0\venv\Scripts & activate & rqd
+cmd /k "cd /d D:\OpenCue\v0.4.14\venv\Scripts & activate & rqd
